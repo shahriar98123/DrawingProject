aa61bf35935e23b03b56d010f984dee91f9d8f9f
Shahriar <ssha7332@gmail.com>
2018-11-07 13:36:20 -0700

    I just tried to use the oulet and it didnt work

---
+++ DrawingProject/View/ArtCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArtCell.swift
+//  DrawingProject
+//
+//  Created by Shams, Shahriar on 11/7/18.
+//  Copyright © 2018 Ctec. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell: UICollectionViewCell
+{
+    
+}
