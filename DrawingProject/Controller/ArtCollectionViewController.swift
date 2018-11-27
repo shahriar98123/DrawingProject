6700e0e1ff75586b549717b12c1db97aa429dd48
Shahriar <ssha7332@gmail.com>
2018-11-27 11:49:02 -0700

    some stuff added.

--- DrawingProject/Controller/ArtCollectionViewController.swift
+++ DrawingProject/Controller/ArtCollectionViewController.swift
@@ -71,6 +71,26 @@ public class ArtCollectionViewController: UICollectionViewController
         
         return artCell
     }
+    public override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+    {
+        let imageView = UIImageView(image: creativeCS[indexPath.row])
+        imageView.frame = self.view.frame
+        imageView.backgroundColor = .black
+        imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = true
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage))
+        imageView.addGestureRecognizer(tap)
+        
+        self.view.addSubview(imageView)
+    }
+    
+    @objc
+    private func dismissFullscreenImage(_ sender: UITapGestureRecognizer)
+    {
+        sender.view?.removeFromSuperview()
+    }
+    
     
     
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
